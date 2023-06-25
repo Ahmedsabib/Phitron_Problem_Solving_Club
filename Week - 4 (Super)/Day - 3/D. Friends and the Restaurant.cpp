89ac88eead209458bb2273c7e01d6b3781e220e1
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2023-06-25 16:03:09 +0600

    Create D. Friends and the Restaurant.cpp

---
+++ Week - 4 (Super)/Day - 3/D. Friends and the Restaurant.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[]) {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int tc;
+  cin >> tc;
+  for (int cs = 0; cs < tc; ++cs) {
+    long long n;
+    cin >> n;
+    vector<long long> a(n), b(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> a[i];
+    }
+    for (int i = 0; i < n; ++i) {
+      cin >> b[i];
+    }
+    vector<long long> c(n);
+    for (int i = 0; i < n; ++i) {
+      c[i] = b[i] - a[i];
+    }
+    sort(c.begin(), c.end());
+    int i = 0, j = n - 1, ans = 0, tot = 0;
+    while (i < j) {
+      tot = c[j] + c[i];
+      if (tot >= 0) {
+        ++ans;
+        ++i;
+        --j;
+      }
+      else if (tot < 0) {
+        ++i;
+      }
+      else {
+        --j;
+      }
+    }
+    cout << ans << '\n';
+  }
+  return 0;
+}
