1261274185ecf2ab674599a925eedc1ea88940c4
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2023-07-07 23:07:43 +0600

    Create B. Dictionary.cpp

---
+++ Week - 4/Day - 11/B. Dictionary.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]"}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+void test_case() {
+	int n;
+	cin >> n;
+	map<string, int> mp;
+	int cnt = 1;
+	for (char a = 'a'; a <= 'z'; ++a) {
+		string s = "";
+		s += a;
+		for (char b = 'a'; b <= 'z'; ++b) {
+			if (a == b) {
+				continue;
+			}
+			s += b;
+			mp[s] = cnt++;
+			s.pop_back();
+		}
+	}
+	while (n--) {
+		string a;
+		cin >> a;
+		cout << mp[a] << '\n';
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+	int T = 1;
+	while (T--) {
+		test_case();
+	}
+	return 0;
+}
