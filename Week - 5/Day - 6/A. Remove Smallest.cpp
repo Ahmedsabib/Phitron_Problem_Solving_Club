91cecfa97ecb98b8dc407465fba2abc34fb85343
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2023-07-13 11:53:19 +0600

    Create A. Remove Smallest.cpp

---
+++ Week - 5/Day - 6/A. Remove Smallest.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    long long int n;
+    while(t--){
+        cin >> n;
+        int a[n];
+        for(int i = 0; i < n; ++i) cin >> a[i];
+        sort(a, a+n);
+        int m = 0;
+        for(int i = 0; i < n-1; ++i)
+        if(a[i+1] - a[i] > 1) m = 1;
+        if(!m) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    return 0;
+}
