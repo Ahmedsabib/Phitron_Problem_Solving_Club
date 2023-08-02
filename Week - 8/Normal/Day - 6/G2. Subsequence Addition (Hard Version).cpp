7811fa5bf50348e85c1668e650f745a24a6e8691
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2023-08-02 14:18:56 +0600

    Create G2. Subsequence Addition (Hard Version).cpp

---
+++ Week - 8/Normal/Day - 6/G2. Subsequence Addition (Hard Version).cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0);
+
+  int tests;
+  cin >> tests;
+  for (int cases = 0; cases < tests; ++cases) {
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    long long prev = 1, ok = 0;
+    for (int i = 0; i < n; ++i) {
+      if (i == 0) {
+        if (a[0] != prev) {
+          ok = 1;
+        }
+      }
+      else {
+        if (a[i] <= prev) {
+          prev += a[i];
+        }
+        else {
+          ok = 1;
+        }
+      }
+    }
+    cout << (ok? "NO\n" : "YES\n");
+  }
+  return 0;
+}
