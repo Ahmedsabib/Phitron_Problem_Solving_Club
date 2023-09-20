de068cf18d146f9ec7e81aa901c44387ecba0ec5
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2023-09-20 21:47:24 +0600

    Create Subarray Sums I.cpp

---
+++ Week - 15/Normal/Day - 6/Subarray Sums I.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void run_case() {
+    int64_t N, M;
+    cin >> N >> M;
+    vector<int> A(N);
+ 
+    for (auto& x: A)
+        cin >> x;
+ 
+    //Two Pointers algorithm
+    int64_t sum = 0, ans = 0;
+ 
+    for (int i = 0, j = 0; i < N; ++i) {
+        sum += A[i];
+ 
+        while (sum > M) {
+            sum -= A[j];
+            ++j;
+        }
+ 
+        if (sum == M)
+            ++ans;
+    }
+ 
+    cout << ans << '\n';
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    
+    int tests = 1;
+ 
+    while (tests-- > 0)
+        run_case();
+ 
+    return 0;
+}
